9e23e39cb684b1a81a60cdc8ebbaef7f7485dc9c
dcuoliveira <dan.cunhaa@gmail.com>
2023-11-26 16:41:56 -0300

    first commit sims

--- src/simulation/GraphSim.py
+++ src/simulation/GraphSim.py
@@ -13,97 +13,104 @@ class GraphSim:
         Returns:
             None
         """
+        
         self.graph_name = graph_name
         self.seed = seed
 
-    def simulate_erdos(self, n: int, prob: float):
+    def simulate_erdos(self, num_nodes: int, prob: float):
         """
         Simulate a random Erdős-Rényi graph.
         
         Args:
-            n: number of nodes.
+            num_nodes: number of nodes.
             prob: probability of edge creation.
             
         Returns:
             networkx graph object.
         """
-        return nx.erdos_renyi_graph(n=n, p=prob, seed=self.seed)
 
-    def simulate_k_regular(self, n: int, k: int):
+        return nx.erdos_renyi_graph(n=num_nodes, p=prob, seed=self.seed)
+
+    def simulate_k_regular(self, num_nodes: int, d: int):
         """
         Simulate a random k-regular graph.
         
         Args:
-            n: number of nodes.
-            k: degree of each node.
+            num_nodes: number of nodes.
+            d: degree of each node.
             
         Returns:
             networkx graph object.
         """
-        return nx.random_regular_graph(d=k, n=n, seed=self.seed)
+        
+        return nx.random_regular_graph(d=d, n=num_nodes, seed=self.seed)
 
-    def simulate_geometric(self, n: int, radius: float):
+    def simulate_geometric(self, num_nodes: int, radius: float):
         """
         Simulate a random geometric graph.
         
         Args:
             n: number of nodes.
-            radius: radius for edge creation.
+            num_nodes: radius for edge creation.
             
         Returns:
             networkx graph object.
         """
-        return nx.random_geometric_graph(n=n, radius=radius, seed=self.seed)
 
-    def simulate_barabasi_albert(self, n: int, m: int):
+        return nx.random_geometric_graph(n=num_nodes, radius=radius, seed=self.seed)
+
+    def simulate_barabasi_albert(self, num_nodes: int, m: int):
         """
         Simulate a random Barabási-Albert preferential attachment graph.
         
         Args:
-            n: number of nodes.
+            num_nodes: number of nodes.
             m: number of edges to attach from a new node to existing nodes.
             
         Returns:
             networkx graph object.
         """
-        return nx.barabasi_albert_graph(n=n, m=m, seed=self.seed)
 
-    def simulate_watts_strogatz(self, n: int, k: int, p: float):
+        return nx.barabasi_albert_graph(n=num_nodes, m=m, seed=self.seed)
+
+    def simulate_watts_strogatz(self, num_nodes: int, k: int, p: float):
         """
         Simulate a random Watts-Strogatz small-world graph.
         
         Args:
-            n: number of nodes.
+            num_nodes: number of nodes.
             k: each node is joined with its k nearest neighbors in a ring topology.
             p: probability of rewiring each edge.
             
         Returns:
             networkx graph object.
         """
-        return nx.watts_strogatz_graph(n=n, k=k, p=p, seed=self.seed)
+
+        return nx.watts_strogatz_graph(n=num_nodes, k=k, p=p, seed=self.seed)
     
-    def simulate_locally_tree_like_graph(self, n: int, avg_degree: float):
+    def simulate_locally_tree_like_graph(self, num_nodes: int, avg_d: float):
         """
         Generate a locally tree-like graph structure using the networkx library.
 
-        :param n: Number of nodes in the graph.
-        :param avg_degree: Average degree (number of edges per node) in the graph.
+        :param num_nodes: Number of nodes in the graph.
+        :param avg_d: Average degree (number of edges per node) in the graph.
 
         :return G: A networkx graph object representing the locally tree-like graph.
         """
+        
         # Create an empty graph
         G = nx.Graph()
 
         # Add nodes to the graph
-        for i in range(n):
+        for i in range(num_nodes):
             G.add_node(i)
 
         # Add edges to create a locally tree-like structure
         for node in G.nodes():
             # Connect each node to a number of other nodes equal to the desired average degree
             # while avoiding self-loops and duplicate edges.
-            while G.degree(node) < avg_degree:
-                target = np.random.choice(n)
+            while G.degree(node) < avg_d:
+                target = np.random.choice(num_nodes)
                 if target != node and not G.has_edge(node, target):
                     G.add_edge(node, target)
 
--- src/simulation/SimGivenGraph.py
+++ src/simulation/SimGivenGraph.py
@@ -1,44 +1,48 @@
 from scipy.stats import multivariate_normal
 import numpy as np
+import networkx as nx
 
 class SimGivenGraph:
     def __init__(self) -> None:
         pass
 
-    def generate_covariance_matrix(self, graph, connected_cov=0.5):
+    def generate_covariance_matrix(self, graph, min_eigenvalue=0.01):
         """
-        Generate a covariance matrix for a multivariate Gaussian distribution
-        based on a given graph structure.
+        Generate a positive semidefinite covariance matrix for a multivariate Gaussian distribution
+        based on a given graph structure, ensuring the matrix is symmetric and positive semidefinite.
 
         :param graph: A networkx graph object.
-        :param connected_cov: The covariance value to use for connected nodes in the graph.
+        :param min_eigenvalue: Minimum eigenvalue to ensure positive semidefiniteness.
         :return: A covariance matrix as a numpy array.
         """
 
-        num_nodes = len(graph.nodes)
-        covariance_matrix = np.zeros((num_nodes, num_nodes))
+        # Create an adjacency matrix from the graph
+        adjacency_matrix = nx.to_numpy_array(graph)
 
-        for i in range(num_nodes):
-            for j in range(num_nodes):
-                if i == j:
-                    covariance_matrix[i, j] = 1  # Variance
-                elif graph.has_edge(i, j):
-                    covariance_matrix[i, j] = connected_cov
+        # Perform spectral decomposition
+        eigenvalues, eigenvectors = np.linalg.eigh(adjacency_matrix)
+
+        # Adjust negative eigenvalues to ensure positive semidefiniteness
+        eigenvalues[eigenvalues < min_eigenvalue] = min_eigenvalue
+
+        # Reconstruct the covariance matrix
+        covariance_matrix = eigenvectors @ np.diag(eigenvalues) @ eigenvectors.T
 
         return covariance_matrix
 
-    def sample_from_graph_structured_gaussian(self, graph, n, connected_cov=0.5):
+    def sample_from_graph_structured_gaussian(self, graph, n, connected_cov=0.5, min_eigenvalue=0.01):
         """
         Sample n observations from a multivariate Gaussian distribution that
-        respects the given graph structure.
+        respects the given graph structure and ensures the covariance matrix is positive semidefinite.
 
         :param graph: A networkx graph object.
         :param n: The number of observations to sample.
         :param connected_cov: The covariance value to use for connected nodes in the graph.
+        :param min_eigenvalue: Minimum eigenvalue to ensure positive semidefiniteness.
         :return: A numpy array of sampled observations.
         """
-
-        covariance_matrix = self.generate_covariance_matrix(graph, connected_cov)
+        
+        covariance_matrix = self.generate_covariance_matrix(graph, connected_cov, min_eigenvalue)
         mean_vector = np.zeros(len(graph.nodes))  # Mean vector (can be adjusted as needed)
 
         # Sample from the multivariate Gaussian distribution
@@ -46,16 +50,24 @@ class SimGivenGraph:
 
         return samples
     
-DEBUG = True
+DEBUG = False
 
 if __name__ == "__main__":
     if DEBUG:
+
+        import sys
+        import os
+        sys.path.append(os.path.join(os.getcwd(), "src"))
+
         from simulation.GraphSim import GraphSim
 
-        gs = GraphSim("test", seed=2294)
-        graph = gs.simulate_locally_tree_like_graph(n=10, avg_degree=2)
-
+        num_nodes = 10
+        avg_d = 2
+        seed = 2294
         n_samples = 100
+
+        gs = GraphSim(f"tree-like-avg{avg_d}", seed=seed)
+        graph = gs.simulate_locally_tree_like_graph(num_nodes=num_nodes, avg_d=avg_d)
+
         sgg = SimGivenGraph()
         samples = sgg.sample_from_graph_structured_gaussian(graph=graph, n=n_samples)
-        samples

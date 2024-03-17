237e069e5e548f8d2ddcc147ef55861cab2d599c
dcuoliveira <dan.cunhaa@gmail.com>
2024-03-17 16:54:31 -0300

    add comment

--- src/data/CitationsLoader.py
+++ src/data/CitationsLoader.py
@@ -5,7 +5,7 @@ import torch
 from torch_geometric.datasets import Planetoid
 import torch_geometric.utils as pyg_utils
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from utils.conn_data import load_pickle, save_pickle
 from utils.SupressPrint import SuppressPrint
